6b1c815f904f777419b163068e15394f872ab1bf
bookkg2 <bookkg2@naver.com>
2019-11-28 17:56:34 +0900

    scroll

--- singsing/post/views.py
+++ singsing/post/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
+
+#url = '/maps/preview/opensearch.xml?hl=ko'
+# response = requests.get(url)
+
+# html = BeautifulSoup(response.text,'html.parser')
+
+# company_list = html.find('button',{"class" })
+# print(company_list)
 # Create your views here.
 def index(request):
 
