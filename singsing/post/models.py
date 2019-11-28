0b26f22ff4f084f80e97ef8ecbd21fa3b9d74d3b
jkim0715 <gjr191@naver.com>
2019-11-28 17:40:18 +0900

    add User, Post, Profile, Comment

--- singsing/post/models.py
+++ singsing/post/models.py
@@ -1,4 +1,49 @@
-from django.db import models
-
+from django.db import models 
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill, ResizeToFit, Thumbnail
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Post(models.Model):
+    # gps_x = models.DecimalField()
+    # gps_y = models.DecimalField()
+    # payment = models.CharField(max_length= 16)
+    #genre = models.TextField()
+    #time = models.DateTimeField()
+    contents = models.TextField()
+    created_date = models.DateTimeField(auto_now_add= True)
+    updated_date = models.DateTimeField(auto_now= True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def comments(self):
+        return Comment.objects.filter(post_id=self.id).order_by('created_date')
+      
+
+
+class Comment(models.Model):
+    contents = models.TextField()
+    created_date = models.DateTimeField(auto_now_add= True)
+    updated_date = models.DateTimeField(auto_now= True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    age = models.IntegerField()
+    birthdate = models.DateField()
+    gender = models.CharField(max_length=16)
+    user_likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="profile_likes")
+
+
+class ProfileImage(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    image = models.ImageField(blank=True)
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[Thumbnail(300,300)],
+        format='JPEG',
+        options={'quality':90}
+    )
+
+    
